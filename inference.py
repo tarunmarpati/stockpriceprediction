ccce660581eb2a1a7b1ee0393a2c86e25a0bb1af
tarunmarpati <85096502+tarunmarpati@users.noreply.github.com>
2021-05-31 12:21:32 +0530

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,12 @@
+import numpy as pd
+import pandas as pd
+from tensorflow import keras
+
+mymodel= keras.models.load_model('model.h5')
+
+real_data= np.array(df[["High","Low","Open","Volume","Adj Close"]])
+real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1],1))
+
+def predict(df):
+    predictions = model.predict(real_data)
+    return list(predictions)
